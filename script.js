fda0182d7da655333eb1aaa5b3a15dcc903234b7
JSINEUNCExcndjkcniNDnk nW <142462189+CodeNinjazs@users.noreply.github.com>
2024-10-21 10:08:58 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,1 @@
-function openInIframe() {
-    const iframe = document.getElementById('contentFrame');
-    iframe.src = 'https://chilibghujugowhmbmulflashe8uofjerf9e.onrender.com'; // Replace with your desired URL
-    iframe.style.display = 'block'; // Show the iframe
-}
+
