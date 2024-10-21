dd62eb6e3692edde4e74a6211d919cb0f7502c2c
JSINEUNCExcndjkcniNDnk nW <142462189+CodeNinjazs@users.noreply.github.com>
2024-10-21 09:55:54 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+function openInIframe() {
+    const iframe = document.getElementById('contentFrame');
+    iframe.src = 'https://chilibghujugowhmbmulflashe8uofjerf9e.onrender.com'; // Replace with your desired URL
+    iframe.style.display = 'block'; // Show the iframe
+}
